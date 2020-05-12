440cdded5629fe0d298b99b45557545dc313a149
ramyadmz <ramyadmz@gmail.com>
2020-05-12 15:16:56 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="todoapp" className="todoapp">
+        <header className="header">
+          <h1>todo</h1>
+          <input
+            type="text"
+            className="new-todo"
+            placeholder="what needs to be done?"
+          />
+        </header>
+        <section className="main">
+          <ul className="todo-list">
+            <li>
+              <div className="view">
+                <input type="checkbox" className="toggle" />
+                <label>new work</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+            <li className="completed">
+              <div className="view">
+                <input type="checkbox" className="toggle" value="on" checked={false} />
+                <label>old work</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
